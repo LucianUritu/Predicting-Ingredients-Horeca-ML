e4c6a5bd6f637cba76ad578f82cf46e79ecd9dc9
Lucian Uritu <lucian@uritu.net>
2026-03-27 15:11:19 +0100

    added ingrediect forecasting script

--- src/features/__init__.py
+++ src/features/__init__.py
@@ -1,5 +1,6 @@
 # src/features/__init__.py
 
 from .buildFeatures import build_features
+from .ingredientForecasting import compute_ingredient_demand
 
-__all__ = ["build_features"]
+__all__ = ["build_features", "compute_ingredient_demand"]
---
+++ src/features/ingredientForecasting.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+def compute_ingredient_demand(predictions_df, recipes_df):
+    
+    # Merge predictions with recipes
+    merged = predictions_df.merge(
+        recipes_df,
+        on="menu_item_id",
+        how="left"
+    )
+    
+    # Compute ingredient quantity
+    merged["ingredient_quality"] = (
+        merged["predicted_quality"] * merged["quantity"]
+    )
+
+    # Aggregate per store + ingredient
+    ingredient_demand = (
+        merged
+        .groupby(["store_id", "ingredient_id", "sales_date"])
+        ["ingredient_quantity"]
+        .sum()
+        .reset_index()
+    )
+
+    return ingredient_demand
