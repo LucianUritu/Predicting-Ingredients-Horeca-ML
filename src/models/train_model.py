d83a63b42ed0172ed3b495d27929ed51adaa174a
Lucian Uritu <lucian@uritu.net>
2026-04-23 13:19:13 +0200

    updated train model

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,11 +1,15 @@
 from sklearn.ensemble import RandomForestRegressor
 
+
 def train_model(X_train, y_train):
     model = RandomForestRegressor(
-        n_estimators     = 200,
-        max_depth       = None,
-        random_state    = 42,
-        n_jobs          = 1
+        n_estimators=300,
+        max_depth=10,
+        min_samples_split=5,
+        min_samples_leaf=2,
+        max_features="sqrt",
+        random_state=42,
+        n_jobs=-1
     )
 
     model.fit(X_train, y_train)
