c77cd3a2a8523c6829d8b70bb3d1a92567373997
Lucian Uritu <lucian@uritu.net>
2026-06-26 12:57:17 +0300

    removed annoying login

--- src/app.py
+++ src/app.py
@@ -47,22 +47,7 @@ def run_pipeline(df):
 
 
 def main():
-    if "logged_in" not in st.session_state:
-        st.session_state["logged_in"] = False
-
-    if not st.session_state["logged_in"]:
-        st.title("Login")
-        username = st.text_input("Username")
-        password = st.text_input("Password", type="password")
-        if st.button("Login"):
-            if check_login(username, password):
-                st.session_state["logged_in"] = True
-                st.success("Login successful!")
-                return
-            else:
-                st.error("Invalid username or password.")
-        return
-
+    
     render_header()
 
     df = load_data()
